4f1431c994c2eb4cceae28b2484be1ddfd196bd9
Alex Wilson <gpg@probablyfine.co.uk>
2021-04-11 21:29:49 +0100

    feat(set1): fixed_xor implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod set1;
 
+use crate::set1::fixed_xor::*;
 use crate::set1::hex_to_base64::*;
 
 fn main() {
-    let input = String::from(
-        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    println!(
+        "{}",
+        fixed_xor(
+            String::from("1c0111001f010100061a024b53535009181c"),
+            String::from("686974207468652062756c6c277320657965")
+        )
     );
-
-    println!("{}", hex_to_base64(input));
 }
--- src/set1/fixed_xor.rs
+++ src/set1/fixed_xor.rs
@@ -1,9 +1,28 @@
 pub fn fixed_xor(one: String, two: String) -> String {
+    let alphabet: Vec<char> = "0123456789abcdef".chars().collect();
+
     if one == two {
         return String::from_utf8(vec![b'0'; one.len()]).unwrap();
     }
 
-    return String::from("")
+    return one
+        .chars()
+        .into_iter()
+        .zip(two.chars().into_iter())
+        .map(|pair| {
+            alphabet
+                .get(hex_value(&alphabet, pair.0) ^ hex_value(&alphabet, pair.1))
+                .unwrap()
+        })
+        .collect::<String>();
+}
+
+fn hex_value(alphabet: &Vec<char>, value: char) -> usize {
+    alphabet
+        .clone()
+        .into_iter()
+        .position(|x| x == value)
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -18,4 +37,11 @@ mod test {
         assert_eq!(fixed_xor(input, output), "000");
     }
 
+    #[test]
+    fn example_input() {
+        let input = String::from("457");
+        let output = String::from("def");
+
+        assert_eq!(fixed_xor(input, output), "9b8");
+    }
 }
