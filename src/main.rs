71cf3c9c4eef0611f7749f18124e914f80b1e7a2
Alex Wilson <gpg@probablyfine.co.uk>
2021-04-11 20:01:25 +0100

    feat(set1): hex_to_base64 works with no padding

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,27 @@ fn main() {
 }
 
 fn bit_for_position(byte: &u8, position: &u8) -> u8 {
-    if byte & (1 << position) == 0 { 0 } else { 1 }
+    if byte & (1 << position) == 0 {
+        0
+    } else {
+        1
+    }
 }
 
 fn hex_to_base64(input: String) -> String {
+    let alphabet: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+        .chars()
+        .collect();
 
-    let alphabet: Vec<char> =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".chars().collect();
-
-    return input.bytes().into_iter()
-        .map(|b| (0..8).into_iter().rev().map(move |c| bit_for_position(&b, &c)) )
+    return input
+        .bytes()
+        .into_iter()
+        .map(|b| {
+            (0..8)
+                .into_iter()
+                .rev()
+                .map(move |c| bit_for_position(&b, &c))
+        })
         .flatten()
         .collect::<Vec<u8>>()
         .chunks(6)
@@ -33,5 +44,3 @@ mod test {
         assert_eq!(hex_to_base64(input), output);
     }
 }
-
-
