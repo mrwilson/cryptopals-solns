631e2392144d13a15d24f91776a13df1b11a3b6b
Alex Wilson <gpg@probablyfine.co.uk>
2021-04-29 23:39:15 +0100

    feat(set1): hamming takes vec<u8> now

--- src/set1/hamming.rs
+++ src/set1/hamming.rs
@@ -1,10 +1,9 @@
 use std::borrow::Borrow;
 
-pub fn hamming(one: &str, two: &str) -> u8 {
+pub fn hamming(one: Vec<u8>, two: Vec<u8>) -> u8 {
     return one
-        .as_bytes()
         .iter()
-        .zip(two.as_bytes().iter())
+        .zip(two.iter())
         .map(|pair| (pair.0 ^ pair.1))
         .map(|byte| u8::count_ones(byte) as u8)
         .sum();
@@ -14,13 +13,20 @@ pub fn hamming(one: &str, two: &str) -> u8 {
 mod test {
     use super::hamming;
 
+    fn to_bytes(input: &str) -> Vec<u8> {
+        return String::from(input).into_bytes();
+    }
+
     #[test]
     fn equality_is_zero() {
-        assert_eq!(hamming("foo", "foo"), 0);
+        assert_eq!(hamming(to_bytes("foo"), to_bytes("foo")), 0);
     }
 
     #[test]
     fn example() {
-        assert_eq!(hamming("this is a test", "wokka wokka!!!"), 37);
+        assert_eq!(
+            hamming(to_bytes("this is a test"), to_bytes("wokka wokka!!!")),
+            37
+        );
     }
 }
