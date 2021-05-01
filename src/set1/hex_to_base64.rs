d20a8a71b0b124b3524c66039c84c104f7c42554
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-01 20:33:50 +0100

    refactor(set1): Decompose `hex_to_base64` to use existing code

--- src/set1/hex_to_base64.rs
+++ src/set1/hex_to_base64.rs
@@ -1,46 +1,8 @@
-fn bit_for_position(byte: &u8, position: &u8) -> u8 {
-    if byte & (1 << position) == 0 {
-        0
-    } else {
-        1
-    }
-}
+use crate::set1::base64::to_base64;
+use crate::set1::hex::hex_value;
 
 pub fn hex_to_base64(input: String) -> String {
-    let hex = "0123456789abcdef".chars();
-
-    let alphabet: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-        .chars()
-        .collect();
-
-    let mut input_as_bits = input
-        .chars()
-        .into_iter()
-        .map(|c| hex.clone().into_iter().position(|r| r == c).unwrap() as u8)
-        .map(|b| {
-            (0..4)
-                .into_iter()
-                .rev()
-                .map(move |c| bit_for_position(&b, &c))
-        })
-        .flatten()
-        .collect::<Vec<u8>>();
-
-    if input_as_bits.len() % 6 != 0 {
-        input_as_bits.extend(vec![0; 6 - (input_as_bits.len() % 6)]);
-    }
-
-    let mut output = input_as_bits
-        .chunks(6)
-        .map(|chunk| chunk.into_iter().fold(0, |acc, i| (acc << 1) + i) as usize)
-        .map(|sextet| alphabet.get(sextet).unwrap())
-        .collect::<String>();
-
-    if output.len() % 4 != 0 {
-        output.extend(vec!["="; 4 - output.len() % 4]);
-    }
-
-    return output;
+    return String::from_utf8(to_base64(hex_value(input))).unwrap();
 }
 
 #[cfg(test)]
