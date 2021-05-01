07ea11be6bcf8e96f2ccd3fc0bd116a74208eba8
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-01 18:02:43 +0100

    refactor(set1): Tidy up `detect_single_byte_xor`

--- src/set1/single_byte_xor.rs
+++ src/set1/single_byte_xor.rs
@@ -7,56 +7,55 @@ pub fn single_byte_xor<T: AsRef<[u8]>>(key: u8, text: T) -> Vec<u8> {
 }
 
 pub fn detect_single_byte_xor(text: Vec<u8>) -> u8 {
-    let etaoin_shrdlu: HashMap<u8, f32> = [
-        ('e', 0.124167f32),
-        ('t', 0.0969225f32),
-        ('a', 0.0820011f32),
-        ('i', 0.0768052f32),
-        ('n', 0.0764055f32),
-        ('o', 0.0714095f32),
-        ('s', 0.0706768f32),
-        ('r', 0.0668132f32),
-        ('l', 0.0448308f32),
-        ('d', 0.0363709f32),
-        ('h', 0.0350386f32),
-        ('c', 0.0344391f32),
-        ('u', 0.028777f32),
-        ('m', 0.0281775f32),
-        ('f', 0.0235145f32),
-        ('p', 0.0203171f32),
-        ('y', 0.0189182f32),
-        ('g', 0.0181188f32),
-        ('w', 0.0135225f32),
-        ('v', 0.0124567f32),
-        ('b', 0.0106581f32),
-        ('k', 0.00393019f32),
-        ('x', 0.00219824f32),
-        ('j', 0.0019984f32),
-        ('q', 0.0009325f32),
-        ('z', 0.000599),
+    let letter_distribution: HashMap<u8, f32> = [
+        ('e', 0.1241),
+        ('t', 0.0969),
+        ('a', 0.0820),
+        ('i', 0.0768),
+        ('n', 0.0764),
+        ('o', 0.0714),
+        ('s', 0.0706),
+        ('r', 0.0668),
+        ('l', 0.0448),
+        ('d', 0.0363),
+        ('h', 0.0350),
+        ('c', 0.0344),
+        ('u', 0.0287),
+        ('m', 0.0281),
+        ('f', 0.0235),
+        ('p', 0.0203),
+        ('y', 0.0189),
+        ('g', 0.0181),
+        ('w', 0.0135),
+        ('v', 0.0124),
+        ('b', 0.0106),
+        ('k', 0.0039),
+        ('x', 0.0021),
+        ('j', 0.0019),
+        ('q', 0.0009),
+        ('z', 0.0005),
     ]
     .iter()
     .map(|pair| (pair.0 as u8, pair.1))
     .collect();
 
-    let mut outputs: Vec<(Vec<u8>, u8, f32)> = (0u8..255)
+    let mut outputs: Vec<(u8, f32)> = (0u8..255)
         .into_iter()
-        .map(|a| {
-            let sbx = single_byte_xor(a, text.clone());
-
+        .map(|char| (char, single_byte_xor(char, &text)))
+        .map(|(char, new_text)| {
             let mut counts: HashMap<u8, f32> = HashMap::new();
 
-            for char in sbx.clone() {
-                *counts.entry(char).or_insert(0f32) += 1f32 / (text.len() as f32);
+            for char in &new_text {
+                *counts.entry(*char).or_insert(0f32) += 1f32;
             }
 
-            (sbx, a, bhattacharya(counts, etaoin_shrdlu.clone()))
+            (char, bhattacharya(counts, letter_distribution.clone()))
         })
         .collect();
 
-    outputs.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap_or(Equal));
+    outputs.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(Equal));
 
-    return outputs.last().unwrap().clone().1;
+    return outputs.last().unwrap().clone().0;
 }
 
 mod test {
