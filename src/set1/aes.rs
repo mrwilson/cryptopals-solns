0bd17182368882a3559ba7638ca4bcd1b54a055e
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-02 20:17:03 +0100

    feat(set1): Detect repeated blocks under AES-ECB

--- src/set1/aes.rs
+++ src/set1/aes.rs
@@ -22,7 +22,7 @@ pub fn decrypt_aes_ecb<T: AsRef<[u8]>, U: AsRef<[u8]>>(key: T, cipher_text: U) -
 mod test {
     use crate::set1::aes::decrypt_aes_ecb;
     use crate::set1::base64::from_base64;
-    use crate::set1::io::read_file;
+    use crate::set1::io::{read_file, split};
     use std::fs::File;
     use std::io::Read;
     use std::str::from_utf8;
@@ -41,5 +41,28 @@ mod test {
         let as_text = from_utf8(output.as_slice()).unwrap();
 
         assert!(as_text.contains("Play that funky music white boy you say it, say it "))
+    }
+
+    #[test]
+    fn find_aes_ecb_encrypted_entry() {
+        let input: Vec<Vec<u8>> = split(read_file("inputs/1_8.txt"), '\n' as u8);
+
+        for ciphertext in input {
+            let decoded = from_base64(ciphertext.clone());
+
+            let mut blocks: Vec<&[u8]> = decoded.chunks(16).collect();
+
+            let total_blocks = blocks.len();
+
+            blocks.sort();
+            blocks.dedup();
+
+            let distinct_blocks = blocks.len();
+
+            if total_blocks != distinct_blocks {
+                assert_eq!(ciphertext, "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a".as_bytes()
+                );
+            }
+        }
     }
 }
--- src/set1/io.rs
+++ src/set1/io.rs
@@ -8,3 +8,36 @@ pub fn read_file(path: &str) -> Vec<u8> {
     file.read_to_string(&mut content).unwrap();
     content.into_bytes()
 }
+
+pub fn split<T: AsRef<[u8]>>(input: T, char: u8) -> Vec<Vec<u8>> {
+    let mut output: Vec<Vec<u8>> = Vec::new();
+    let mut intermediate: Vec<u8> = Vec::new();
+
+    for c in input.as_ref() {
+        if *c == char {
+            output.push(intermediate.clone());
+            intermediate.clear();
+        } else {
+            intermediate.push(*c);
+        }
+    }
+
+    output.push(intermediate);
+
+    output
+}
+
+#[cfg(test)]
+mod test {
+    use crate::set1::io::split;
+
+    #[test]
+    fn example_1() {
+        let input = "ab\ncd";
+
+        let output = split(input, '\n' as u8);
+
+        assert_eq!(output[0], "ab".as_bytes());
+        assert_eq!(output[1], "cd".as_bytes());
+    }
+}
