c1610c0b54dcabf37e37ea305494b75e6cbe360c
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-03 22:25:27 +0100

    refactor(set2): Extract `has_repeated_blocks`

--- src/set1/aes.rs
+++ src/set1/aes.rs
@@ -115,11 +115,25 @@ mod cbc {
 
         output
     }
+
+    pub fn has_repeated_blocks(decoded: Vec<u8>) -> bool {
+        let mut blocks: Vec<&[u8]> = decoded.chunks(16).collect();
+
+        let total_blocks = blocks.len();
+
+        blocks.sort();
+        blocks.dedup();
+
+        let distinct_blocks = blocks.len();
+
+        total_blocks != distinct_blocks
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::set1::aes;
+    use crate::set1::aes::cbc::has_repeated_blocks;
     use crate::set1::aes::encryption_oracle;
     use crate::set1::base64::from_base64;
     use crate::set1::io::{read_file, split};
@@ -149,16 +163,7 @@ mod test {
         for ciphertext in input {
             let decoded = from_base64(ciphertext.clone());
 
-            let mut blocks: Vec<&[u8]> = decoded.chunks(16).collect();
-
-            let total_blocks = blocks.len();
-
-            blocks.sort();
-            blocks.dedup();
-
-            let distinct_blocks = blocks.len();
-
-            if total_blocks != distinct_blocks {
+            if has_repeated_blocks(decoded) {
                 assert_eq!(ciphertext, "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a".as_bytes()
                 );
             }
@@ -210,16 +215,7 @@ mod test {
         for _ in 1..1000 {
             let (using_cbc, output) = encryption_oracle(&input);
 
-            let mut blocks: Vec<&[u8]> = output.chunks(16).collect();
-
-            let total_blocks = blocks.len();
-
-            blocks.sort();
-            blocks.dedup();
-
-            let distinct_blocks = blocks.len();
-
-            assert_eq!(using_cbc, total_blocks == distinct_blocks)
+            assert_ne!(using_cbc, has_repeated_blocks(output))
         }
     }
 }
