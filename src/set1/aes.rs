9a98bf718c3ba7d0b38a47bbbc25901f1b769e52
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-03 22:15:25 +0100

    feat(set2): Create encryption oracle between AES ECB/CBC

--- src/set1/aes.rs
+++ src/set1/aes.rs
@@ -1,5 +1,31 @@
 extern crate openssl;
 
+use crate::set1::aes;
+use rand::{thread_rng, Rng};
+
+pub fn encryption_oracle<T: AsRef<[u8]>>(input: T) -> (bool, Vec<u8>) {
+    fn random_bytes(length: usize) -> Vec<u8> {
+        return (0..length).map(|_| thread_rng().gen::<u8>()).collect();
+    };
+
+    let key = random_bytes(16);
+
+    let padding_size: usize = thread_rng().gen_range(5..10);
+
+    let mut padded_input = random_bytes(padding_size);
+    padded_input.extend(input.as_ref());
+    padded_input.extend(random_bytes(padding_size));
+
+    let use_cbc: bool = thread_rng().gen();
+
+    if use_cbc {
+        let iv = random_bytes(16);
+        (use_cbc, aes::cbc::encrypt(key, padded_input, iv))
+    } else {
+        (use_cbc, aes::ecb::encrypt(key, padded_input))
+    }
+}
+
 mod ecb {
     use super::openssl::symm::{Cipher, Crypter, Mode};
 
